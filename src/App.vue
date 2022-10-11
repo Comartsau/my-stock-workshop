7a5bbc3e097ba63468926ad5b006ff7dce04bb81
Comartsau <comartsau@gmail.com>
2022-10-11 15:58:23 +0700

    edit app.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <h1>comartsau</h1>
+    <v-btn color="info">text</v-btn>
+    <v-icon>email</v-icon>
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
