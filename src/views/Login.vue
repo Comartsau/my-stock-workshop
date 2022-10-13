e7ce9659ebff8058ca95317de01a9ba2427999cd
Comartsau <comartsau@gmail.com>
2022-10-13 11:29:48 +0700

    template register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,41 @@
 <template>
-<v-container>
-    <h1>Login</h1>
-</v-container>
+  <v-container>
+    <v-row class="justify-center">
+      <v-card style="width: 400px">
+        <v-img class="white--text align-end " src="@/assets/login_header.jpg" height="200px" >
+            <v-card-title primary-title>
+                Login
+            </v-card-title>
+        </v-img>
+        <v-card-text>
+          <v-form>
+            <!-- Username -->
+            <v-text-field name="Username" label="Username" id="Username" />
+
+            <!-- Password -->
+            <v-text-field name="Password" label="Password" id="Password" 
+            :append-icon="isShowPassword ? 'visibility' : 'visibility_off'" 
+            @click:append="isShowPassword = !isShowPassword" :type="isShowPassword ? 'text' : 'password'"/>
+
+            <v-row class="justify-space-between px-3">
+                <v-btn text @click.prevent="$router.push('/register') " >Register</v-btn>
+                <v-btn color="success">Login</v-btn>
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+    data () {
+        return {
+            isShowPassword:false
+        }
+    }
+};
 </script>
 
 <style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,68 @@
 <template>
-  <h1>Resgister</h1>
+  <v-container>
+    <v-row class="justify-center">
+      <v-card style="width: 400px">
+        <v-img
+          class="white--text align-end"
+          src="@/assets/login_header.jpg"
+          height="200px"
+        >
+          <v-card-title primary-title> Register </v-card-title>
+        </v-img>
+        <v-card-text>
+          <v-form @submit.prevent="submit">
+            <!-- Username -->
+            <v-text-field
+              v-model="account.username"
+              name="Username"
+              label="Username"
+              id="Username"
+              :rules="usernameRules"
+            />
+
+            <!-- Password -->
+            <v-text-field
+              v-model="account.password"
+              name="Password"
+              label="Password"
+              id="Password"
+              :append-icon="isShowPassword ? 'visibility' : 'visibility_off'"
+              @click:append="isShowPassword = !isShowPassword"
+              :type="isShowPassword ? 'text' : 'password'"
+              :rules="passwordRules"
+            />
+            <span>{{ account }}</span>
+            <v-row class="justify-space-between px-3">
+              <v-btn text @click.prevent="$router.back()">Cancle</v-btn>
+              <v-btn type="submit" color="success">Confirm</v-btn>
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      isShowPassword: false,
+      account: {
+        username: "",
+        password: "",
+      },
+      usernameRules:[v1=>!!v1 || "Username is erquired"],
+      passwordRules:[v1=>!!v1 || "Password is erquired", v2=>!!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(v2) || "Minimum eight characters, at least one letter and one number:"]
+    };
+  },
+  methods: {
+    submit() {
+      alert(JSON.stringify(this.account));
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
